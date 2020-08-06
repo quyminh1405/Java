e0be8001724564628acd92949cf3e54995d12f61
quyminh1405 <56126685+quyminh1405@users.noreply.github.com>
2020-08-06 10:19:08 +0700

    Generic Methods

---
+++ 7. GenericMethods/src/com/company/Main.java
@@ -0,0 +1,27 @@
+package com.company;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+
+    static Integer[] intArray = { 4, 2, 0, 6, 9};
+    static String[] stringArray = {"fu", "ck", "you"};
+    static Boolean[] boolArray = {true, false, true};
+
+    public static <T> List<T> arrayToList(T[] array, List<T> list) {
+        for (T object : array) {
+            list.add(object);
+        }
+        return list;
+    }
+
+    public static void main(String[] args) {
+	    List<String> stringList = arrayToList(stringArray, new ArrayList<>());
+	    List<Integer> intList = arrayToList(intArray, new ArrayList<>());
+	    List<Boolean> boolList = arrayToList(boolArray, new ArrayList<>());
+
+	    /*The <T> helps us with not getting ClassCastException when we apply the wrong type like this*/
+        //List<String> stringList2 = arrayToList(intArray, new ArrayList<>());
+    }
+}
